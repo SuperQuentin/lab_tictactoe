c6ca8471b53876d4aedbd811ea6046bd67b594e2
SuperQuentin <quentin.aellen@infrack.ch>
2022-09-27 16:12:50 +0200

    refactor: add function to check win state and return wining player

--- main.cpp
+++ main.cpp
@@ -2,27 +2,17 @@
 
 using namespace std;
 
-int main() {
-
-    const char tableau[9] = {
-    'x', NULL, NULL,
-    NULL,'x',NULL,
-    NULL,NULL,'x',
-    };
-
+char checkWin(const char *tableau) {
     const short win[8][3] = {
-     {0,1,2},
-     {2,5,8},
-     {6,7,8},
-     {0,3,6},
-     {3,4,5},
-     {1,4,7},
-     {0,4,8},
-     {2,4,6}
+            {0,1,2},
+            {2,5,8},
+            {6,7,8},
+            {0,3,6},
+            {3,4,5},
+            {1,4,7},
+            {0,4,8},
+            {2,4,6}
     };
-
-    char winPlayer = NULL;
-
 
     for(int x = 0; x < sizeof(tableau);x++) {
         auto [index1, index2, index3] = win[x];
@@ -32,11 +22,26 @@ int main() {
         }
 
         if(tableau[index1] == tableau[index2] && tableau[index2] == tableau[index3]) {
-            winPlayer = tableau[index1];
-            cout << "Tu as gagn\x82 < " << winPlayer << " > you did it" << endl;
-            return true;
+            return tableau[index1];;
         }
     }
+    return NULL;
+}
+
+int main() {
+    const char tableau[9] = {
+    'x', NULL, NULL,
+    NULL,'x',NULL,
+    NULL,NULL,'x',
+    };
+
+    char winPlayer = checkWin(tableau);
+
+    if(winPlayer != NULL)
+        cout << "tu es bon " << winPlayer << endl;
 
     return 0;
 }
+
+
+
