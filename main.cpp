84ad177cb32f42520e885b4b731f53170da3cd3b
SuperQuentin <quentin.aellen@infrack.ch>
2022-09-29 14:19:19 +0200

    feat: add function to splitCoords from str input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <regex>
 
 using namespace std;
 
@@ -8,6 +10,21 @@ struct Vector2
     int x = 0;
     int y = 0;
 };
+
+Vector2 splitCoords(string& rawInput) {
+    smatch coords;
+    struct Vector2 extractedCoords;
+
+    // Search all corresponding patterns into the input string ex: 3-9, 6-8
+    if (regex_match(rawInput, coords,regex("([0-9]-[0-9])"))) {
+        string rawCoords = coords[coords.size()-1]; // Get last given cords if there is multiples
+        extractedCoords.x = stoi(rawCoords.substr(0,1));
+        extractedCoords.y = stoi(rawCoords.substr(2,1));
+    }
+
+    return extractedCoords;
+}
+
 
 char checkWin(const char *tableau)
 {
